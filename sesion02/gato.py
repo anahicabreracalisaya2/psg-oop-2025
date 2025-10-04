47d18a97bc291fb7587ffbe4221849b19f18f157
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-10-04 11:56:29 -0400

    Sesión 02

---
+++ sesion02/gato.py
@@ -0,0 +1,12 @@
+
+class Gato:
+    def __init__(self, color):
+        self.color = color
+
+
+pantera = Gato("negro")
+snowball = Gato("blanco")
+print(pantera.color)
+print(snowball.color)
+pantera = Gato("negro")
+snowball = Gato("blanco")
---
+++ sesion02/michi_constructor.py
@@ -0,0 +1,22 @@
+class Gato:
+    especie = "felino"
+    def __init__(self, color, genero, edad, castrado):
+        self.color = color
+        self.genero = genero
+        self.edad = edad
+        self.castrado = castrado
+# Instanciar
+pelusa = Gato("Negro", "hembra", 2, False)
+miauricio = Gato("Naranja", "macho", 1, True)
+# Mostrar atributos
+print("Pelusa: ",pelusa.especie)
+print(pelusa.color)
+print(pelusa.genero)
+print(pelusa.edad)
+print(pelusa.castrado)
+print("Miauricio: ",miauricio.especie)
+print(miauricio.color)
+print(miauricio.genero)
+print(miauricio.edad)
+print(miauricio.castrado)
+#python michi_constructor.py
