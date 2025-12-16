abd90b8820b9deaeac4e70ae5adfcccfe9d9eb4b
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-12-16 15:56:22 -0400

    Cuentita

--- retos_sesion_04/ejercicio_01.py
+++ retos_sesion_04/ejercicio_01.py
@@ -1,27 +1,8 @@
 class Cuenta:
-    def __init__(self, numero_cuenta, nombre_titular,saldo_ini):
-        self.__numero_cuenta = numero_cuenta
-        self.__saldo = saldo_ini
-        self._nombre_titular = nombre_titular
-
-    @property
-    def numero_cuenta(self):
-        return self.__numero_cuenta
-    @property
-    def saldo(self):
-        return self.__saldo
-
-    @property
-    def nombre_titular(self):
-        return self._nombre_titular
-
-    @nombre_titular.setter
-    def nombre_titular(self, nuevo_nombre):
-        if self._nombre_titular != nuevo_nombre:
-            self._nombre_titular = nuevo_nombre
-            print("✅ Nombre del titular actualizado correctamente.")
-        else:
-            print("⚠️ Nombre inválido. No se realizaron cambios.")
+    def __init__(self, nombre_titular):
+        self.__numero_cuenta = "123567901"
+        self.__saldo = 0
+        self.nombre_titular = nombre_titular
 
     def depositar(self, monto):
         if monto > 0:
@@ -38,15 +19,34 @@ class Cuenta:
         else:
             self.__saldo -= monto
             print(f"🏧 Retiro exitoso. Su saldo es: {self.__saldo}")
+    def saldo(self):
+        print(f"💵 Su saldo actual es: {self.__saldo}")
+        return self.__saldo
+    def numero_cuenta(self):
+        print(f"🔢 Su número de cuenta es: {self.__numero_cuenta}")
+        return self.__numero_cuenta
+    def nombre_titular(self):
+        print(f"👤 Titular de la cuenta: {self.nombre_titular}")
+        return self.nombre_titular
+    def cambio_titular(self, nuevo_nombre):
+        if self.nombre_titular != nuevo_nombre:
+            self.nombre_titular = nuevo_nombre
+            print("✅ Nombre del titular actualizado correctamente.")
+        else:
+            print("⚠️ Nombre inválido. No se realizaron cambios.")
+
+
 
 
 # Implementando la clase
-cuenta = Cuenta("12345678", "Anahi Cabrera", 500)
+cuenta = Cuenta("Anahi Cabrera")
+print("Informaciom de la cuenta:")
 print("Número de cuenta:", cuenta.numero_cuenta)
 print("Titular:", cuenta.nombre_titular)
 print("Saldo inicial:", cuenta.saldo)
 cuenta.depositar(200)
 cuenta.retirar(100)
 cuenta.retirar(700)  # Error
-cuenta.nombre_titular = "Cielo Calisaya"
+print("Cambiando de titular...")
+cuenta.cambio_titular("Cielo Calisaya")
 print("Nuevo titular:", cuenta.nombre_titular)
